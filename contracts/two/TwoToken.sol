d2d686100092fd9ec9daa50ab0a5633d00956310
twoMeme <two@twochoice.org>
2022-01-21 12:30:40 +0800

    upgrade twoToken test

--- contracts/two/TwoToken.sol
+++ contracts/two/TwoToken.sol
@@ -29,6 +29,6 @@ contract TwoToken is ERC20PermitUpgradeable, IKaki, AccessControlUpgradeable {
     }
 
     function version() public pure returns (uint256) {
-        return 2;
+        return 3;
     }
 }
--- utils/contract.ts
+++ utils/contract.ts
@@ -2,7 +2,7 @@ import { deployments, ethers, network } from 'hardhat';
 import {
   CaptainClaim__factory,
   BlindBox, KakiCaptain__factory, KakiGarden__factory, KakiTicket, MysteryBox__factory
-  , AddressList__factory, MockChainLink__factory, MockToken__factory, Ticket__factory, OpenBox__factory, KakiSquidGame, KakiSquidGame__factory, BlindBox__factory, KakiTicket__factory, ERC20__factory, Kaki__factory, ClaimLock__factory, KakiNoLoss, KakiNoLoss__factory, Tools__factory,
+  , AddressList__factory, MockChainLink__factory, MockToken__factory, Ticket__factory, OpenBox__factory, KakiSquidGame, KakiSquidGame__factory, BlindBox__factory, KakiTicket__factory, ERC20__factory, TwoToken__factory, ClaimLock__factory, KakiNoLoss, KakiNoLoss__factory, Tools__factory,
 } from '~/typechain';
 
 import bsctest from './contractAddress/bscTest'
@@ -195,8 +195,8 @@ export async function claimLockContract(signerIndex = 0) {
   return ClaimLock__factory.connect(contractAddress.claimLock, await getSigner(signerIndex));
 }
 
-export async function kakiContract(signerIndex = 0) {
-  return Kaki__factory.connect(contractAddress.kaki, await getSigner(signerIndex));
+export async function twoTokenContract(signerIndex = 0) {
+  return TwoToken__factory.connect(contractAddress.two, await getSigner(signerIndex));
 }
 
 
