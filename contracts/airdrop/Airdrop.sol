cb81e33b093f75912e53f52848dcfeffda3edc0f
twoMeme <two@twochoice.prg>
2022-01-26 19:45:07 +0800

    fixed bug

--- contracts/airdrop/Airdrop.sol
+++ contracts/airdrop/Airdrop.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/utils/cryptography/draft-EIP712Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/cryptography/ECDSAUpgradeable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "../interfaces/IAirdrop.sol";
 
