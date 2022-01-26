b5333c5565746f7a20572607aca99fa59d8d340e
twoMeme <two@twochoice.prg>
2022-01-26 17:12:28 +0800

    presale update

--- contracts/preSale/PreSale.sol
+++ contracts/preSale/PreSale.sol
@@ -2,11 +2,14 @@
 pragma solidity ^0.8.0;
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import "../interfaces/IPresale.sol";
 
-contract PreSale is Ownable {
-    uint256 public two_LeftPart = 4400;
+contract Presale is IPresale, Ownable {
+    uint256 public twoLeftPart = 4400;
     uint256 public constant LOWER_LIMIT = 22 * 1e18;
     uint256 public constant TWO_EACHPART = 22222 * 1e16;
+    uint256 public constant SALE_PERIOD = 86400;  //24h
+    uint256 public constant SALE_STARTSTAMP = 1643882400; //UTC 2022-2-3 10:00
     address public constant TWO_ADDRESS = 0xd91cfd064F4C1a9ee91Fc58fCa671c4cF6A68ADB;
     address _admin;
     IERC20 public _two;
@@ -23,29 +26,34 @@ contract PreSale is Ownable {
         _;
     }
 
-    function sale() public payable {
+    modifier isStart() {
+        require(block.timestamp > SALE_STARTSTAMP && block.timestamp < SALE_STARTSTAMP + SALE_PERIOD, "END");
+        _;
+    }
+
+    function sale() public payable override isStart {
         require(msg.value >= LOWER_LIMIT, "INVALID AMOUNT.");
-        require(two_LeftPart != 0, "SOLD OUT.");
+        require(twoLeftPart != 0, "SOLD OUT.");
         uint256 part = (msg.value - msg.value % 22) / 22;
         require(saleList[msg.sender] + part < 10, "UPPER LIMIT");
 
-        if (part > two_LeftPart) {
-            uint256 amount = (part - two_LeftPart) * LOWER_LIMIT;
+        if (part > twoLeftPart) {
+            uint256 amount = (part - twoLeftPart) * LOWER_LIMIT;
             (bool success, ) = msg.sender.call { value: amount } (new bytes(0));
             require(success, "! safe transfer FTM");
         } else {
             _two.transfer(msg.sender, part * TWO_EACHPART);
-            two_LeftPart -= part;
+            twoLeftPart -= part;
             saleList[msg.sender] += part;
         }
         
     }
 
-    function getLeftAmount() public view returns(uint256) {
-        return two_LeftPart;
+    function getLeftAmount() public view override returns(uint256) {
+        return twoLeftPart;
     }
 
-    function withdraw() public onlyAdmin {
+    function withdraw() public override onlyAdmin {
         uint256 amount = address(this).balance;
         (bool success, ) = msg.sender.call{ value: amount } (new bytes(0));
         require(success, "! safe transfer FTM");
