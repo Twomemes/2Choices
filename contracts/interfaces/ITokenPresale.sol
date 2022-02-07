0a06e54ebcbe764f4ebbb8410d923e8355bf57e1
twoMeme <two@twochoice.prg>
2022-02-07 18:56:43 +0800

    token presale rewrite

--- contracts/interfaces/ITokenPresale.sol
+++ contracts/interfaces/ITokenPresale.sol
@@ -2,11 +2,18 @@
 pragma solidity ^0.8.0;
 
 interface ITokenPresale {
-    event Sale(address indexed account, uint256 part);
+    event Sale(address indexed account);
+    event WhiteListSale(address indexed account);
+
+    function whiteListSale(
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external payable;
 
     function sale() external payable;
-    function getLeftAmount() external view returns(uint256);
     function withdraw() external;
     function withdrawTwo() external;
-
+    function getLeftAmount() external view returns(uint256);
+    function checkCurrentPeriod() external view returns(uint256);
 }
--- contracts/tokenPresale/TokenPresale.sol
+++ contracts/tokenPresale/TokenPresale.sol
@@ -2,26 +2,30 @@
 pragma solidity ^0.8.0;
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/cryptography/ECDSAUpgradeable.sol";
 import "../interfaces/ITokenPresale.sol";
 
 contract TokenPresale is ITokenPresale, OwnableUpgradeable {
-    uint256 public twoLeftPart ;
+    using ECDSAUpgradeable for bytes32;
+
+    uint256 public twoLeftPart;
     uint256 public saleStartStamp ; //UTC 2022-2-3 10:00  1643882400
     uint256 public salePeriod;  //24h  86400
-    uint256 public constant LOWER_LIMIT = 22 * 1e18;
-    uint256 public constant TWO_EACHPART = 222222 * 1e15;
-    address public constant TWO_ADDRESS = 0x7e09c5dE33C464394eaAa199Adc4b310A7ccBe6B;//0x9F1851f29374eFb292cFa78503fc02A9b640c45b
-    address _admin;
+    uint256 public wlSalePeriod;
+    uint256 public constant SINGLEPART = 11 * 1e18;
+    uint256 public constant TWO_EACHPART = 2222.22 * 1e18;
+    address public _admin;
+    address public _signer;
     IERC20 public _two;
-    mapping(address => uint256) public saleList;
-    
+    mapping(address => uint256) public saleList;    
 
     function initialize(IERC20 twoadd) public initializer {
         __Ownable_init();
-        twoLeftPart = 200;
+        twoLeftPart = 440;
         _two = twoadd;
-        saleStartStamp=1644224400;
-        salePeriod = 86400*3;  
+        saleStartStamp = 1644224400;
+        salePeriod = 2400;  //60 * 20 * 3
+        wlSalePeriod = 1200;
     }
 
     receive() external payable {}
@@ -31,34 +35,58 @@ contract TokenPresale is ITokenPresale, OwnableUpgradeable {
         _;
     }
 
-    modifier isStart() {
-        require(block.timestamp > saleStartStamp && block.timestamp < saleStartStamp + salePeriod, "END");
+    modifier isNoneWLStart() {
+        require(block.timestamp > saleStartStamp + wlSalePeriod && block.timestamp < saleStartStamp + salePeriod, "SALE HAD END.");
         _;
     }
 
-    function sale() public payable override isStart {
-        require(msg.value >= LOWER_LIMIT, "INVALID AMOUNT.");
+    function sale() public payable override isNoneWLStart {
         require(twoLeftPart != 0, "SOLD OUT.");
-        uint256 part = msg.value  / 22 ether;
-        require(saleList[msg.sender] + part <= 10, "UPPER LIMIT");
+        require(msg.value == SINGLEPART, "INVALID AMOUNT.");
+        require(saleList[msg.sender] == 0, "HAD BOUGHT.");
+        _sale(msg.sender);
 
-        if (part > twoLeftPart) {
-            uint256 amount = (part - twoLeftPart) * LOWER_LIMIT;
-            _two.transfer(msg.sender, twoLeftPart * TWO_EACHPART * 22);
-            (bool success, ) = msg.sender.call { value: amount } (new bytes(0));
-            require(success, "! safe transfer FTM");
-        } else {
-            _two.transfer(msg.sender, part * TWO_EACHPART * 22);
-            twoLeftPart -= part;
-            saleList[msg.sender] += part;
-        }
-        
+        emit Sale(msg.sender);
     }
+
+    function whiteListSale(
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) public payable override{
+        uint256 currentTime = block.timestamp;
+        require(currentTime > saleStartStamp && currentTime < saleStartStamp + wlSalePeriod, "WHITELIST SALE END OR NOT START.");
+        require(saleList[msg.sender] == 0, "HAD BOUGHT.");
+        require(msg.value == SINGLEPART, "INVALID AMOUNT.");
+        require(
+            keccak256(abi.encode(msg.sender)).toEthSignedMessageHash().recover(v, r, s) == _signer,
+            "claim:Invalid signarure"
+        );
+        _sale(msg.sender);
+
+        emit Sale(msg.sender);
+    }
+
+    //===================================================VIEW======================================= */
 
     function getLeftAmount() public view override returns(uint256) {
         return twoLeftPart;
     }
+ 
+    function checkCurrentPeriod() public view override returns(uint256) {
+        uint256 currentTime = block.timestamp;
+        if (currentTime > saleStartStamp && currentTime < saleStartStamp + wlSalePeriod) return 1; 
+        else return 2;
+    }
 
+    //===================================================INTERNAL======================================= */
+    function _sale(address receiver) internal {
+        _two.transfer(receiver, TWO_EACHPART);
+        twoLeftPart -= 1;
+        saleList[receiver] += 1;
+    }
+
+    //===================================================ADMIN======================================= */
     function withdraw() public override onlyAdmin {
         require(_admin != address(0), "INVALID admin");
 
@@ -90,6 +118,6 @@ contract TokenPresale is ITokenPresale, OwnableUpgradeable {
     }
 
     function version() public pure returns (uint256) {
-        return 7;
+        return 0;
     }
 }
