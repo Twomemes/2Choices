8c48b0c42d8b4add75ba3b9fc7591e4812905d35
twoMeme <two@twochoice.org>
2022-02-08 19:04:43 +0800

    script

--- scripts/tokenPresale/setPara.ts
+++ scripts/tokenPresale/setPara.ts
@@ -3,10 +3,10 @@ import { tokenPresaleContract} from '../../utils/contract';
 
 (async () => {
   const contract = await tokenPresaleContract();
-  const ax = await contract.setSaleStartStamp(1644224400);
+  const ax = await contract.setSaleStartStamp(1644303600);
 
   console.log(ax.hash);
-  const ax2 = await contract.setSalePeriod(86400*3);
+  //const ax2 = await contract.setSalePeriod(86400*3);
 
-  console.log(ax2.hash);
+  //console.log(ax2.hash);
 })();
