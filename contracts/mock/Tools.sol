830f88411f4f207674a469499869e2a976695411
twoMeme <two@twochoice.org>
2022-02-14 20:19:32 +0800

    lp apr
    Signed-off-by: twoMeme <two@twochoice.org>

--- contracts/mock/Tools.sol
+++ contracts/mock/Tools.sol
@@ -6,39 +6,111 @@ import {IVault} from "../interfaces/IVault.sol";
 import {IFairLaunch} from "../interfaces/IFairLaunch.sol";
 import "../interfaces/IERC20.sol";
 
+import {IGarden} from "../interfaces/IGarden.sol";
+
+import "../interfaces/IPancakePair.sol";
+
 contract Tools is WithAdminRole {
+    IPancakePair public twoFtmLp;
+    IERC20 public two;
+    IERC20 public wftm;
+    IGarden public garden;
+
+    uint256 public ftmPrice; // * 10000
+
     function initialize() public initializer {
         __WithAdminRole_init();
+    }
+
+    function twoPrice() public view returns (uint256) {
+        uint256 twoBl = two.balanceOf(address(twoFtmLp));
+        uint256 ftmBl = wftm.balanceOf(address(twoFtmLp));
+
+        // u/ftm * ftm / two
+
+        return (ftmPrice * ftmBl) / twoBl;
+    }
+
+    function twoLpPrice() public view returns (uint256) {
+        uint256 twoBl = two.balanceOf(address(twoFtmLp));
+        uint256 ftmBl = wftm.balanceOf(address(twoFtmLp));
+        uint256 lpBl = twoFtmLp.totalSupply();
+        // (two price * two +   wftm price * wftm )/ lp valumn
+        return (twoPrice() * twoBl + ftmPrice * ftmBl) / lpBl;
+    }
+
+    function apr(uint256 pid) public view returns (uint256) {
+        if (pid == 0) {
+            return ftmApr2();
+        } else if (pid == 1) {
+            return lpApr2();
+        }
+    }
+
+    function apr2(uint256 pid) public view returns (uint256) {
+        if (pid == 0) {
+            return ftmApr();
+        } else if (pid == 1) {
+            return lpApr();
+        }
+    }
+
+    function lpApr() public view returns (uint256) {
+        return
+            ((garden.daylyReward(1) * twoPrice()) * 365 * 10000) / (twoFtmLp.balanceOf(address(garden)) * twoLpPrice());
+    }
+
+    function lpApr2() public view returns (uint256) {
+        uint256 twoBl = two.balanceOf(address(twoFtmLp));
+        return (garden.daylyReward(1) * 365 * 10000) / (twoBl * 2);
+    }
+
+    function ftmApr2() public view returns (uint256) {
+        uint256 twoBl = two.balanceOf(address(twoFtmLp));
+        uint256 ftmBl = wftm.balanceOf(address(twoFtmLp));
+
+        uint256 staking = wftm.balanceOf(address(garden));
+        return (garden.daylyReward(0) * 365 * 10000) / ((staking * twoBl) / ftmBl);
+    }
+
+    function ftmApr() public view returns (uint256) {
+        return ((((garden.daylyReward(0) * twoPrice()) * 365 * 10000) / (wftm.balanceOf(address(garden)))) * ftmPrice);
+    }
+
+    function setGarden(IGarden _garden) public {
+        garden = _garden;
+    }
+
+    function setTwoFtmLp(IPancakePair _twoFtmLp) public {
+        twoFtmLp = _twoFtmLp;
+    }
+
+    function setTwo(IERC20 _two) public {
+        two = _two;
+    }
+
+    function setWftm(IERC20 _wftm) public {
+        wftm = _wftm;
+    }
+
+    function setFtmPrice(uint256 _price) public {
+        ftmPrice = _price;
     }
 
     function approve(IERC20 token, address spender) public {
         token.approve(spender, type(uint256).max);
     }
 
-    function deposit(
-        IVault vault,
-        uint256 amount,
-        bool isNative
-    ) public payable {
-        vault.deposit{value: isNative ? amount : 0}(amount);
-    }
-
-    function withdraw(IVault vault, uint256 amount) public {
-        vault.withdraw(amount);
-    }
-
-    function depositAndWithdraw(
-        IVault vault,
-        uint256 amount,
-        bool isNative
-    ) public payable {
-        deposit(vault, amount, isNative);
-        withdraw(vault, amount);
+    function spiltETH(address[] memory recipients, uint256[] memory amounts) public payable {
+        uint256 len = recipients.length;
+        for (uint256 i; i < len; i++) {
+            recipients[i].call{value: amounts[i]}(new bytes(0));
+        }
     }
 
     receive() external payable {}
 
     function version() public pure returns (uint256) {
-        return 6;
+        return 12;
     }
 }
