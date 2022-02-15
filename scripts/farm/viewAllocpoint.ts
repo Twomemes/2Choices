b7099affb4386a1976ca6f58cab9f62685762844
kaki-admin <two@twochoice.org>
2022-02-15 12:55:53 +0800

    d

--- scripts/farm/viewAllocpoint.ts
+++ scripts/farm/viewAllocpoint.ts
@@ -4,11 +4,11 @@ import { parseEther } from 'ethers/lib/utils';
 (async () => {
   const farm = await farmContract();
 
-  const squid = await farm._squidGameAllocPoint();
+  const squid = await farm.virtualPoolInfo();
+  console.log(squid);
+  //const pool0 = await farm._poolInfo(0);
+  //const pool1 = await farm._poolInfo(1);
 
-  const pool0 = await farm._poolInfo(0);
-  const pool1 = await farm._poolInfo(1);
 
-
-  console.log(`total: ${squid.add(pool0.allocPoint).add(pool1.allocPoint).toNumber()} squid: ${squid.toNumber()}  pool0: ${pool0.allocPoint.toNumber()}  pool1: ${pool1.allocPoint.toNumber()}`);
+  //console.log(`total: ${squid.add(pool0.allocPoint).add(pool1.allocPoint).toNumber()} squid: ${squid.toNumber()}  pool0: ${pool0.allocPoint.toNumber()}  pool1: ${pool1.allocPoint.toNumber()}`);
 })();
